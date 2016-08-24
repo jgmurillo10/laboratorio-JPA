b92a4c3b242973a73b4b05f21ce303df55f3e8bf
Juan Murillo <jg.murillo10@uniandes.edu.co>
2016-08-24 13:20:50 -0500

    -JPA functional -Exception NotAuthorized import error

--- src/main/java/com/example/services/LogInService.java
+++ src/main/java/com/example/services/LogInService.java
@@ -8,16 +8,19 @@ import com.example.PersistenceManager;
 import com.example.models.Competitor;
 import com.example.models.CompetitorDTO;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.persistence.*;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import org.json.simple.JSONObject;
+//import javax.ws.rs.NotAuthorizedException;
 
 /**
  *
@@ -28,4 +31,44 @@ import org.json.simple.JSONObject;
 public class LogInService {
     @PersistenceContext(unitName = "Laboratorio-JPA")
     EntityManager entityManager;    
+    @PostConstruct
+    public void init() {
+        try {
+            entityManager = PersistenceManager.getInstance().getEntityManagerFactory().createEntityManager();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    } 
+    
+    @POST
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response loginCompetitor(JSONObject json, @PathParam("id") Long id){// throws NotAuthorizedException{
+        
+        ArrayList<String> values=new ArrayList<String>();
+        Iterator ite= json.values().iterator();
+        while(ite.hasNext()){
+            values.add(ite.next().toString());
+        }
+        
+        String password= values.get(0);
+        String address=values.get(1);
+        String rta;
+        Competitor competitor2=entityManager.find(Competitor.class, id);
+               
+        if(competitor2.getAddress().equals(address)&
+            password.equals(competitor2.getPassword())){
+            rta="Esta haciendo el login";
+        }else{
+            rta="address or password invalid";
+//            throw new NotAuthorizedException("address or password invalid");
+        }        
+        
+        
+        
+      System.out.println("before return");
+ 
+            return Response.status(200).header("Access-Control-Allow-Origin", "*").entity(rta).build();
+        
+    }
 }
